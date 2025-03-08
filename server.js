32ca08775a20843aa3d1c4eb69ca2d2d9af3f0f0
sagar-techy <142889336+sagar-techy@users.noreply.github.com>
2025-03-09 00:08:31 +0530

    Second

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 io.on('connection', socket => {
- 
-    console.log('New client connected');
 
+    console.log('New client connected');
     socket.on('disconnect', () => {
         console.log('Client disconnected');
     });
-    socket.on("some-event", () => {
+    socket.on("some-event", (msg) => {
+        console.log(msg)
         console.log("some-event occured");
+        socket.emit('client-event',msg)
     })
 });
 
